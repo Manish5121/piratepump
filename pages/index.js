8e9d62a1d084e5ef2abe40c66b393ed14970e86e
Manish5121 <manishgambhirrao21@gmail.com>
2023-04-23 17:56:20 +0530

    Add Buy me a coffee

--- pages/index.js
+++ pages/index.js
@@ -95,6 +95,7 @@ const Home = () => {
         >
           <div className="badge">
             <p>build by @Micky__21_</p>
+            <script data-name="BMC-Widget" data-cfasync="false" src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js" data-id="micky21" data-description="Support me on Buy me a coffee!" data-message="" data-color="#BD5FFF" data-position="Right" data-x_margin="18" data-y_margin="18"></script>
           </div>
         </a>
       </div>
